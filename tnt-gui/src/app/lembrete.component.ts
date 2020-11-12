b6f16edb7a57c9b15bc1f978e59203be92726d7b
Marcelo Valois <mmv4@cin.ufpe.br>
2020-11-12 02:48:20 -0300

    Esqueletos criados

---
+++ tnt-gui/src/app/lembrete.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: '',
+    templateUrl: './lembrete.component.html',
+    styleUrls: ['./lembrete.component.css']
+  })
+
+  export class LembreteComponent implements OnInit {
+      constructor() {
+
+      }
+
+      ngOnInit() {
+          
+      }
+  }
