276c5eb15a8a8b4cbe61f552c2dd26f25f61f67b
Humberto <hcct@cin.ufpe.br>
2020-11-12 11:30:22 -0300

    Adicionando esqueleto de Eventos + Aplicação da GUI

---
+++ tnt-gui/src/app/events.ts
@@ -0,0 +1,24 @@
+import { newArray } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+
+
+
+export class EventosComponent implements OnInit{
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+    title = 'Eventos'
+}
