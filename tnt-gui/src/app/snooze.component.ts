b598233604eeab1eae17d8073e83c96a6bc802eb
LinJTF <felipejtlin@gmail.com>
2020-11-12 02:50:50 -0300

    Esqueleto do Snoozing(GUI) + Snoozing(Serviço)

---
+++ tnt-gui/src/app/snooze.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@AppComponent({
+    selector: 'app-root',
+    templateUrl: './snoozing.component.html',
+    styleUrls: ['./snoozing.component.css']
+})
+
+export class SnoozeComponent implements OnInit {
+    constructor() {
+
+    }
+
+    ngOnInit(){
+        
+    }
+}
