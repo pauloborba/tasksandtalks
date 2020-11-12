31cdfba070cf162d7632a81c8c71b873f45e576d
matheus29t <matheus29t@gmail.com>
2020-11-12 02:51:30 -0300

    esqueleto do Contexto(GUI) + contexto(Serviço)

---
+++ tnt-gui/src/app/contexto.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: '',
+    templateUrl: './contexto.component.html',
+    styleUrls: ['./contexto.component.css']
+  })
+  export class ContextoComponent implements OnInit {
+    constructor() {
+
+    }
+   
+    ngOnInit() {
+
+    }
+  }
