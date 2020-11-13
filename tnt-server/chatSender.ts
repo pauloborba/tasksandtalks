752703ae99f33d5ebb602df4e3c4f0e0ff0e8993
Tales Tomaz Alves <54077178+tta13@users.noreply.github.com>
2020-11-13 11:29:32 -0300

    Removendo métodos desnecessários em ChatSender

--- tnt-server/chatSender.ts
+++ tnt-server/chatSender.ts
@@ -2,14 +2,9 @@ import { Chat } from '../common/chat';
 import { ChatThread } from '../common/chatThread';
 
 export class ChatSender{
-    chatThreads: ChatThread[] = [];
 
     constructor(){
 
-    }
-
-    createChatThread(threadName: string, firstMessage: Chat): ChatThread{
-        return null;
     }
 
     sendChat(chatMessage: Chat, thread: ChatThread): void{
