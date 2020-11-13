06627a925945cc413c34537cf7b8ed3312153a88
José Bezerra <jbmn2@cin.ufpe.br>
2020-11-13 15:53:43 -0300

    fix(EmailSenderService): attributes and methods implemented

--- tnt-server/EmailSenderService.ts
+++ tnt-server/EmailSenderService.ts
@@ -1,1 +1,9 @@
-export class EmailSenderService { }
+export class EmailSenderService {
+    public sender: string;
+
+    constructor(sender: string) {
+        this.sender = sender;
+    }
+
+    public send(subject: string, recipient: string, content: string) { }
+}
