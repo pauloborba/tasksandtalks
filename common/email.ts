6e2bb8fce53898188937d9540d818b45aebb669f
led <luiseduardomartinsalves@gmail.com>
2020-11-13 01:22:25 -0300

    Criação do esqueleto da classe Email

---
+++ common/email.ts
@@ -0,0 +1,11 @@
+export class Email{
+    sender: string;
+    receiver: string;
+    messageContent: string;
+    date: Date;
+
+    constructor(sender: string, receiver: string, messageContent: string, date: Date){
+
+    }
+
+}
