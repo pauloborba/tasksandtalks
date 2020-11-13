ad6f3d260644302cf4e250e5a8ca1459459d5067
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-11-13 01:21:54 -0300

    Criação da classe repositorioDeProjetos

---
+++ tnt-server/repositorioDeProjetos.ts
@@ -0,0 +1,6 @@
+export class repositorioDeProjetos {
+    listaDeProjetos : Projeto[] = [];
+    adicionarProjeto(projeto : Projeto){};
+    removerProjeto(nomeProjeto : string){};
+    atualizarAtributos(){};
+} 
