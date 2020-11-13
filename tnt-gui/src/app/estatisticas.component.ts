3552ef3d799d05facd770972e393f9a8680a09d4
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-11-13 01:23:00 -0300

    Criacao da tela estatisticas

---
+++ tnt-gui/src/app/estatisticas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: '',
+    templateUrl: './estatisticas.component.html',
+    styleUrls: ['./estatisticas.component.css']
+  })
+  export class estatisticasComponent implements OnInit {
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+  }
