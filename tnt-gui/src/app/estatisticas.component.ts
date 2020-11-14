4558d418629b2477f349a23bfbfdc94b4c8a416d
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-11-13 23:27:55 -0300

    atribuição de nome 'estatisticas' ao selector

--- tnt-gui/src/app/estatisticas.component.ts
+++ tnt-gui/src/app/estatisticas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: '',
+    selector: 'estatisticas',
     templateUrl: './estatisticas.component.html',
     styleUrls: ['./estatisticas.component.css']
   })
