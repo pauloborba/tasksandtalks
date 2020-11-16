f4de1ae9aa6bc6a8c65f76e7c43ca9cede371b01
Marcelo Valois <mmv4@cin.ufpe.br>
2020-11-16 15:12:32 -0300

    correção do servidor com exclusão do lembrete.ts e adição do calendarImport.ts

--- tnt-gui/src/app/lembrete.component.ts
+++ tnt-gui/src/app/lembrete.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: '',
+    selector: 'lembrete',
     templateUrl: './lembrete.component.html',
     styleUrls: ['./lembrete.component.css']
   })
