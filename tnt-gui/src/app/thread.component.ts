44b5720d62de2eb172d209a8dad45db31339d314
José Bezerra <jbmn2@cin.ufpe.br>
2020-11-19 15:16:51 -0300

    feat(EmailThread): stub states implemented

--- tnt-gui/src/app/thread.component.ts
+++ tnt-gui/src/app/thread.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Email } from '../../../common/Email'
 
+interface ThreadEmailI {
+  id: number;
+  email: Email;
+  reply_id: number;
+}
+
 @Component({
-  selector: 'app-contexto',
+  selector: 'app-thread',
   templateUrl: './thread.component.html',
   styleUrls: ['./thread.component.css']
 })
+
 export class ThreadComponent implements OnInit {
   constructor() { }
 
-  emails: Email[] = []
+  emails: ThreadEmailI[] = []
 
   ngOnInit() {
     for (let i = 0; i < 10; i++) {
-      this.emails.push(new Email('José Bezerra <jbmn2@cin.ufpe.br>', 'phmb@cin.ufpe.br', `Teste ${i + 1}`, 'Apenas um Teste'))
+      const email = new Email('José Bezerra <jbmn2@cin.ufpe.br>', 'phmb@cin.ufpe.br', `Teste ${i + 1}`, 'Apenas um Teste');
+      const id = i;
+      const reply_id = i - 1;
+      this.emails.push({
+        id,
+        email,
+        reply_id
+      })
     }
   }
 }
