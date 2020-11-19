c4bd74731e13cc34be8ae9b55a633a54ffa270cc
José Bezerra <jbmn2@cin.ufpe.br>
2020-11-19 12:24:54 -0300

    fix(Email): attributes order changed for more readability

--- common/Email.ts
+++ common/Email.ts
@@ -4,11 +4,11 @@ export class Email{
     content: string;
     subject: string;
 
-    constructor(sender: string, recipient: string, content: string, subject: string){
+    constructor(sender: string, recipient: string, subject: string, content: string){
         this.sender = sender;
         this.recipient = recipient;
-        this.content = content;
         this.subject = subject;
+        this.content = content;
     }
 
 }
