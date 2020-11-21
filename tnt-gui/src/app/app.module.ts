277277cd758306bca5231e33488a3a58aef80b51
tta13 <tta@cin.ufpe.br>
2020-11-21 18:26:19 -0300

    [stub]Adicionando rota para a página de projetos

--- tnt-gui/src/app/app.module.ts
+++ tnt-gui/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjetosComponent } from './projetos.component'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'projetos',
+        component: ProjetosComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
