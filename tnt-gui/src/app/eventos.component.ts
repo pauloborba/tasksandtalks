c091669791efa1c48673618462d9fb0c0862fdc8
Humberto <hcct@cin.ufpe.br>
2020-11-24 20:49:40 -0300

    Adicinando botões com funcionalidades temporarias de STUBS

--- tnt-gui/src/app/eventos.component.ts
+++ tnt-gui/src/app/eventos.component.ts
@@ -1,3 +1,4 @@
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
@@ -13,11 +14,44 @@ import { EventService } from './event.service';
 
 
 export class EventosComponent{
-  event: Event = {nome: "", data: "", tarefa: ""};
-  events: Event[] = [];
+  event: Event = {nome: "", data: "", hora: "", tarefa: ""};
+  events: Event[] = [{nome: "Preparar Aula de Requisitos", data: "15/12/2020", hora: "08:00", tarefa: "Aulas - ESS"},{nome: "Preparar seminario da CBsoft", data: "20/12/2020", hora: "12:00", tarefa: "CBsoft"}];
+  mostrarLista: boolean = false; 
+  dataInput: string;
+  linkCalendar: string;
+  tarefa1: string;
+  tarefa2: string;
 
-  gravar(a: Event): void {
-    this.events.push(a);
-    this.event = {nome: "", data: "", tarefa: ""};
- }
+
+  listarEventos(): void {
+   this.mostrarLista = !this.mostrarLista;
+  }
+  
+  associarEventosPorData(data: string): void{
+  
+    for(let i=0;i<this.events.length;i++){
+      if(this.events[i].data == data){
+        // Faz a associação de cada evento à tarefa especificada
+        this.mostrarLista = !this.mostrarLista;
+        // 
+      }
+    }
+  this.dataInput = "";
+  this.tarefa1 = "";
+  }
+
+  associarEventosPorLink(link: string): void{
+
+    if( link == "google.calendar/evento1_dasdadasd"){
+      // Faz a associação de cada evento à tarefa especificada
+      this.mostrarLista = !this.mostrarLista;
+      // 
+    }
+    else{}
+  this.linkCalendar = "";
+  this.tarefa2 = "";
+  }
+
+
+
 }
