8f7233da4f98856e546cba88d09ba1c3066fc9a9
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-11-24 19:04:16 -0300

    [ProjetosArquivados] adição de gráfico para exibir a quantidade de projetos arquivados ao longo do tempo

--- tnt-gui/src/app/projetosArquivados.component.ts
+++ tnt-gui/src/app/projetosArquivados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {EstatisticasService} from './estatisticas.service';
 
 @Component({
     selector: 'ProjetosArquivados',
@@ -6,11 +7,65 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./projetosArquivados.component.css']
   })
   export class ProjetosArquivadosComponent implements OnInit {
-    constructor() {
+    constructor(private estatisticasService: EstatisticasService) {}
 
+    mensagemSobreArquivados: string;
+    porcentagemArquivados: string;
+    listaPorcQuant: number[];
+    listaChavesQuant: (string[] | number[])[];
+    listaMeses: (string[] | number[]);
+    listaArquivadosPorMes: (string[] | number[]);
+    //atributos do grafico
+    barChartOptions;
+    barChartType;
+    barChartLegend;
+    barChartData;
+    barChartLabels;
+
+    ajustaDatas(datas){
+      for(let i=0; i<datas.length; i++){
+        datas[i] = datas[i].substr(4,2) + '/' + datas[i].substr(0,4);
+      }
+    }
+
+    inicializarGrafico(at:(string[] | number[])[]){
+      this.listaMeses = at[0];
+      this.ajustaDatas(this.listaMeses);
+      this.listaArquivadosPorMes = at[1];
+
+      this.barChartOptions = {
+        scaleShowVerticalLines: false,
+        responsive: true
+      };
+      this.barChartLabels = this.listaMeses;
+      this.barChartType = 'line';
+      this.barChartLegend = true;
+      this.barChartData = [
+        {data: this.listaArquivadosPorMes, label: 'Projetos Arquivados'}
+      ];
+    }
+
+    inicializar(lst:number[]){
+      this.listaPorcQuant = lst;
+      if(this.listaPorcQuant[1] == 0){
+        this.mensagemSobreArquivados = "Não há projetos arquivados";
+      }else{
+        this.mensagemSobreArquivados = "Há " + this.listaPorcQuant[1].toString() + " projeto(s) arquivado(s)"
+      }
+      if(this.listaPorcQuant[0]) this.porcentagemArquivados = this.listaPorcQuant[0].toString() + '%';
+      else this.porcentagemArquivados = '';
     }
 
     ngOnInit() {
-
+      this.estatisticasService.getArquivados()
+      .subscribe(
+          arq => this.inicializarGrafico(arq),
+          msg => { alert(msg.message); }
+      );
+      this.estatisticasService.getPorcentagem(false)
+      .subscribe(
+          lst => this.inicializar(lst),
+          msg => { alert(msg.message); }
+      );
     }
   }
