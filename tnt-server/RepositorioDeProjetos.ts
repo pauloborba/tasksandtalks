c11eebaab041f2a13bbb8e8be3f2aa9fd1d464c4
Amanda Moraes <mandynhamoraes@hotmail.com>
2020-11-23 23:17:09 -0300

    Adição do método getPorcentagem à classe RepositorioDeProjetos para retornar a porcentagem de projetos ativos ou arquivados

--- tnt-server/RepositorioDeProjetos.ts
+++ tnt-server/RepositorioDeProjetos.ts
@@ -17,6 +17,21 @@ export class RepositorioDeProjetos {
         this.listaDeProjetos = [];
     }
 
+    getPorcentagem(returnAtivos:boolean) : number{
+        var ativos = 0, arquivados = 0;
+        for(let proj of this.listaDeProjetos){
+            if(proj.arquivado) arquivados++;
+            else ativos++;
+        }
+        var porcAtivos = null, porcArquivados = null;
+        if(this.listaDeProjetos.length > 0){
+            porcAtivos = ativos*100/this.listaDeProjetos.length;
+            porcArquivados = arquivados*100/this.listaDeProjetos.length;
+        }
+        if(returnAtivos) return porcAtivos;
+        else return porcArquivados;
+    }
+
     getArquivados() : Map<string, number>{
         var retorno = null;
         //atualiza map de projetos arquivados por mes (caso nao tenha os meses mais recentes)
