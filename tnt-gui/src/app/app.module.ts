2ed2109ba2b1bd11973dc7d2d6f07bb5875d8f56
Eduarda Santos <mesps@cin.ufpe.br>
2020-11-25 20:42:37 -0300

    Adiconando botao de projetos na GUI

--- tnt-gui/src/app/app.module.ts
+++ tnt-gui/src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ProjetosComponent } from './projetos.component';
+import { ProjetoService } from './projeto.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'projetos',
+        component: ProjetosComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [ProjetoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
