9dbcba199538eb67bad83a11304749c52b6cb306
Humberto <hcct@cin.ufpe.br>
2020-11-25 16:53:17 -0300

    versão inicial do servidor

--- tnt-server/tnt-server.ts
+++ tnt-server/tnt-server.ts
@@ -3,12 +3,22 @@ import bodyParser = require("body-parser");
 
 var tntserver = express();
 
-var allowCrossDomain = function(req: any, res: any, next: any) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-tntserver.use(allowCrossDomain);
+//var allowCrossDomain = function(req: any, res: any, next: any) {
+//    res.header('Access-Control-Allow-Origin', "*");
+//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//    res.header('Access-Control-Allow-Headers', 'Content-Type');
+//    next();
+//}
+//tntserver.use(allowCrossDomain);
 
-tntserver.use(bodyParser.json());
+//tntserver.use(bodyParser.json());
+
+var tarefas = [{nome:'Aula'}]
+
+tntserver.get('/', function(req,res){
+	res.send(tarefas);
+})
+
+tntserver.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
