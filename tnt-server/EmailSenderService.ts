a16439f86efa2a31450ef75016f95364c40bb0ab
José Bezerra <jbmn2@cin.ufpe.br>
2020-11-26 13:10:23 -0300

    feat(EmailSenderService): class tests implemented

--- tnt-server/EmailSenderService.ts
+++ tnt-server/EmailSenderService.ts
@@ -4,12 +4,18 @@ import { Email } from '../common/Email'
 
 export class EmailSenderService {
     public async execute(email: Email) {
-        const sendEmail = await Mail.sendMail({
-            from: `<${email.sender}>`,
-            to: `${email.recipient}`,
-            subject: `${email.subject}`,
-            text: `${email.content}`,
-        })
-        return sendEmail
+        const { sender, recipient, subject, content } = email;
+        if (sender && recipient && subject && content) {
+            const sendEmail = await Mail.sendMail({
+                from: `<${email.sender}>`,
+                to: `${email.recipient}`,
+                subject: `${email.subject}`,
+                text: `${email.content}`,
+            })
+            return sendEmail
+        }
+        else {
+            throw new Error("Dados incompletos para o envio de email");
+        }
     }
 }
---
+++ tnt-server/spec/tnt-server.spec.ts
@@ -0,0 +1,8 @@
+import request = require("request-promise");
+
+var base_url = "http://localhost:3000/";
+
+describe("O servidor", () => {
+
+
+})
