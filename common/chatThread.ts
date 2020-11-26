37e42dc4c4ece45371f0b66ac28eee73c2dcba74
LinJTF <felipejtlin@gmail.com>
2020-11-26 09:28:45 -0300

    Adicionando métodos para instanciar Snooze

--- common/chatThread.ts
+++ common/chatThread.ts
@@ -5,13 +5,28 @@ export class ChatThread extends Contexto{
     threadName: string;
     threadChats: Chat[] = [ {sender: "juan",messageContent:"ola",sendDate:new Date()},
                             {sender: "julia",messageContent:"oie",sendDate:new Date()}];
+    activateSnooze: Chat[] = [];
+    bufferSnooze: Chat[] = [];
+    horaDoSnooze: Date = new Date();
+    snoozing: Date;
 
    constructor(){
        super();
    }
 
    addChat(newChat: Chat): void{
-    this.threadChats.push(newChat)
+    let aux = new Date();
+    let now = aux.getTime(); 
+    let snoozeTime = new Date(this.horaDoSnooze.toString());
+    let snoozingTime = snoozeTime.getTime();
+    if(now >= snoozingTime){
+        this.threadChats.push(newChat);
+        console.log("adicionou no principal");
+    }
+    else{
+        this.bufferSnooze.push(newChat);
+        console.log(this.bufferSnooze);
+    }
    }
 
    read(): void{
@@ -20,5 +35,40 @@ export class ChatThread extends Contexto{
 
    getThreadChats(): Chat[]{
     return this.threadChats;
+   }
+   
+   applySnoozing(){
+       return this.bufferSnooze;
+   }
+
+   addSnoozing(newSnoozing: Chat){
+    //this.snoozing = newSnoozing;
+    //return newSnoozing;
+    console.log(newSnoozing);
+    this.horaDoSnooze = new Date(newSnoozing.sendDate.toString());
+    console.log(this.horaDoSnooze);
+    setInterval(this.checkSnoozing, 1000);
+   }
+
+   checkSnoozing = () => {
+        if (this.bufferSnooze) {
+
+            let aux = new Date();     //aqui pega a hora de agora
+            let now = aux.getTime();  //aqui converte essa hora de agora para um inteiro
+            let snoozeTime = new Date(this.horaDoSnooze.toString());
+            let snoozingTime = snoozeTime.getTime(); //aqui pega a hora do snooze         snoozeTime = horaDoSnooze.getTime()
+
+            if(now >= snoozingTime) {
+                for(let a of this.bufferSnooze){
+                    this.threadChats.push(a);  //se sim, adiciona tudo que tá no buffer de Snooze no thread chats 
+                } //aqui tu compara: now > snoozetime ?  
+                console.log(this.bufferSnooze);
+                console.log(this.threadChats);
+                this.bufferSnooze.splice(0, this.bufferSnooze.length);   
+                console.log(this.bufferSnooze);
+                
+                       
+            }
+        }
    }
 }
