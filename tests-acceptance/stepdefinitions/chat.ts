c3a8f485d0534d8b166efb9e69c0c0fdd9bef189
matheus29t <matheus29t@gmail.com>
2020-11-26 01:34:15 -0300

    testes de GUI com todos os cenários

--- tests-acceptance/stepdefinitions/chat.ts
+++ tests-acceptance/stepdefinitions/chat.ts
@@ -1,18 +1,25 @@
 import { defineSupportCode } from 'cucumber';
 import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+import request = require("request-promise");
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
 let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
 
-let sameMsg = ((elem, msg) => elem.element(by.name('msglist')).getText().then(text => text === msg));
+let sameMsg = ((elem, msg) => elem.element(by.name('msgList')).getText().then(text => text === msg));
 let sameName = ((elem, name) => elem.element(by.name('nameList')).getText().then(text => text === name));
 
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
 
+var base_url = "http://localhost:3000/";
+
 async function sendMessage(msg) {
     await $("textarea[name='writeMessage']").sendKeys(<string> msg);
     await element(by.buttonText('Enviar')).click();
+}
+
+async function assertTamanhoEqual(set,n) {
+    await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
 }
 
 async function login(name) {
@@ -26,6 +33,12 @@ async function loginAndMessage(name, msg) {
     await element(by.buttonText('Enviar')).click();
 }
 
+async function assertElementsWithSameMsg(n,msg) {
+    var allchats : ElementArrayFinder = element.all(by.name('chatlist'));
+    var same = allchats.filter(elem => sameMsg(elem,msg));
+    await assertTamanhoEqual(same,n); 
+}
+
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am at the task context page$/, async () => {
         await browser.get("http://localhost:4200/");
@@ -33,17 +46,56 @@ defineSupportCode(function ({ Given, When, Then }) {
         await $("a[name='contexto']").click();
     })
 
+    Given(/^I am at the task context page after sending a message$/, async () => {
+
+    })
+
     Given(/^I can see that I am not logged in$/, async () => {
         var login_status = element(by.name('login')).getText();
-        expect(login_status).to.eventually.equal('Não logado');
+        await expect(Promise.resolve(login_status)).to.eventually.equal('Não logado');
     });
 
+    Given(/^I can see that I am logged in as "([^\"]*)"$/, async (name) => {
+        var login_name = element(by.name('loginName')).getText();
+        await login_name;
+        await expect(login_name).to.eventually.equal(name);
+    });
+    
     When(/^I try to send the chat message "([^\"]*)" with "([^\"]*)" in the name field$/, async (msg, name) => {
         await loginAndMessage(name, msg);
+    });
+
+    When(/^I try to send the chat message "([^\"]*)"$/, async (msg) => {
+        await sendMessage(msg);
+    });
+
+    When(/^I try to send a chat message with no content$/, async () => {
+        await element(by.buttonText('Enviar')).click();
+    });
+
+    When(/^I try to send a chat message with no content with "([^\"]*)" in the name field$/, async (name) => {
+        await login(name);
     });
 
     Then(/^I can see the chat message "([^\"]*)" that was sent by "([^\"]*)" in the chat thread$/, async (msg, name) => {
         var allchats : ElementArrayFinder = element.all(by.name('chatList'));
         allchats.filter(elem => pAND(sameMsg(elem,msg),sameName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+
+    Then(/^I cannot see the the chat message "([^\"]*)" in the chat thread$/, async (msg) => {
+        await request.get(base_url + "chat")
+                .then(body => 
+                   expect(body.includes(`"messageContent":"${msg}"`)).to.equal(false));
+    });
+
+    Then(/^I cannot see an empty message in the chat thread$/, async () => {
+        await request.get(base_url + "chat")
+                .then(body => 
+                   expect(body.includes(`"messageContent":""`)).to.equal(false));
+    });
+
+    Then(/^I cannot see an empty message that was sent by "([^\"]*)" in the chat thread$/, async (name) => {
+        var allchats : ElementArrayFinder = element.all(by.name('chatList'));
+        allchats.filter(elem => pAND(sameMsg(elem,""),sameName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.not.equal(1));
     });
 })
